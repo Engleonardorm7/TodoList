aca0d79a9255ccc06817ea0d6b3b66ea5c940e4b
Leonardo <leonardorm7@hotmail.com>
2024-09-19 07:01:37 +0200

    localStorage implemented

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App(){
-  const [task, setTask] = useState('');
-  const [tasks, setTasks] = useState([]);
+
+  const [task, setTask] = useState();
+  const [tasks, setTasks] = useState(()=>{
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    return savedTasks;
+  });
+  
+  //to save tasks in the local storage
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+  
+
 
   const handleInputChange = (textInput) => {
     setTask(textInput.target.value);
@@ -11,18 +23,21 @@ function App(){
   };
 
   const handleAddTask = () => {
-    setTasks([...tasks,task]);
-    setTask('');
+    if(task){
+      const newTasks=[...tasks,task]
+      setTasks(newTasks);
+      setTask('');
+    }
   };
 
   return(
     <div className='App'>
       <h1>To-Do List</h1>
       <input
-      type='text'
-      value={task}
-      onChange={handleInputChange}
-      placeholder='Add a new task'
+        type='text'
+        value={task}
+        onChange={handleInputChange}
+        placeholder='Add a new task'
       />
       <button onClick={handleAddTask}>Add Task</button>
 
